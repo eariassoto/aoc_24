8b4689c28fec5cd81e6ed4223f6fb6c48cfbd884
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2024-12-03 07:29:36 +0100

    Day 3 task 2 solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,8 @@ import kotlin.math.absoluteValue
 
 fun main() {
     val day03Input: String = File("day3.txt").readDay03()
-    solveDay03Task01(day03Input)
+    //solveDay03Task01(day03Input)
+    solveDay03Task02(day03Input)
 
     //val day02Input: List<Day02Report> = File("day2.txt").readDay02()
     //solveDay02Task01(day02Input)
@@ -22,15 +23,63 @@ fun File.readDay03(): String {
     return result!!
 }
 
-fun solveDay03Task01(input: String)
-{
+fun solveDay03Task01(input: String) {
     val mulRegex = Regex("mul\\((?<val1>\\d{1,3}),(?<val2>\\d{1,3})\\)")
     val result = mulRegex.findAll(input).map {
         it.groups["val1"]?.value!!.toInt() *
-        it.groups["val2"]?.value!!.toInt()
+                it.groups["val2"]?.value!!.toInt()
     }.sum()
 
     println(result)
+}
+
+fun solveDay03Task02(input: String) {
+    val mulRegex = Regex("mul\\((?<val1>\\d{1,3}),(?<val2>\\d{1,3})\\)")
+    val doRegex = Regex("do\\(\\)")
+    val dontRegex = Regex("don't\\(\\)")
+
+    // state
+    var mulEnabled = true
+    var total = 0
+
+    var i: Int = 0
+    while (true) {
+        val nextDo = doRegex.find(input, i)
+        val nextDont = dontRegex.find(input, i)
+        val nextMul = mulRegex.find(input, i)
+
+        if (nextDo != null
+            && (nextDont == null || nextDo.range.first < nextDont.range.first)
+            && (nextMul == null || nextDo.range.first < nextMul.range.first)
+        ) {
+            //println("found next do")
+            mulEnabled = true
+            i = nextDo.range.last + 1
+        } else if (nextDont != null
+            && (nextDo == null || nextDont.range.first < nextDo.range.first)
+            && (nextMul == null || nextDont.range.first < nextMul.range.first)
+        ) {
+            //println("found next dont")
+            mulEnabled = false
+            i = nextDont.range.last + 1
+        } else if (nextMul != null
+            && (nextDo == null || nextMul.range.first < nextDo.range.first)
+            && (nextDont == null || nextMul.range.first < nextDont.range.first)
+        ) {
+            //println("found next mul")
+            if (mulEnabled) {
+                val result: Int =
+                    nextMul.groups["val1"]?.value!!.toInt() *
+                            nextMul.groups["val2"]?.value!!.toInt()
+                total += result
+            }
+            i = nextMul.range.last + 1
+        } else {
+            break
+        }
+    }
+
+    println(total)
 }
 
 @JvmInline
@@ -52,7 +101,7 @@ fun Day02Report.isSafe(): Boolean {
     val isDescending = this.l.windowed(2).all { (first, second) ->
         first - second >= 0
     }
-    if(!isAscending && !isDescending){
+    if (!isAscending && !isDescending) {
         return false
     }
     val checkDistances = this.l.windowed(2).all { (first, second) ->
@@ -69,13 +118,11 @@ fun Day02Report.combinationsByRemovingOne(): List<Day02Report> {
     }
 }
 
-fun solveDay02Task01(input: List<Day02Report>)
-{
+fun solveDay02Task01(input: List<Day02Report>) {
     println(input.count(Day02Report::isSafe))
 }
 
-fun solveDay02Task02(input: List<Day02Report>)
-{
+fun solveDay02Task02(input: List<Day02Report>) {
     println(input.count() {
         it.combinationsByRemovingOne().any(Day02Report::isSafe)
     })
@@ -94,8 +141,7 @@ fun File.readDay01(): Day01Input {
     return Day01Input(listA, listB)
 }
 
-fun solveDay01Task01(input: Day01Input)
-{
+fun solveDay01Task01(input: Day01Input) {
     val listA: MutableList<Int> = input.listA.toMutableList()
     val listB: MutableList<Int> = input.listB.toMutableList()
     listA.sort()
@@ -105,8 +151,7 @@ fun solveDay01Task01(input: Day01Input)
     println(result)
 }
 
-fun solveDay01Task02(input: Day01Input)
-{
+fun solveDay01Task02(input: Day01Input) {
     val listBCount: Map<Int, Int> = input.listB.groupingBy { it }.eachCount()
     val result: Int = input.listA.sumOf { it * listBCount.getOrDefault(it, 0) }
     println(result)
